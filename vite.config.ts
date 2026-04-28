df3081f2f139238a8fca1e4769cb727b2185f285
坚登 <jiandeng@jiandengdeMac-mini.local>
2026-04-28 16:17:37 +0800

    chore: udpate vite config

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,8 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: 'index.html'
+        main: 'index.html',
+        privacy: 'privacy.html'
       }
     }
   }
