287dc6a57a37f09fb9957d539123948e4e98bcad
坚登 <jiandeng@jiandengdeMac-mini.local>
2026-04-28 16:57:52 +0800

    feat: add changelog.html

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,8 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: 'index.html',
-        privacy: 'privacy.html'
+        privacy: 'privacy.html',
+        changelog: 'changelog.html'
       }
     }
   }
